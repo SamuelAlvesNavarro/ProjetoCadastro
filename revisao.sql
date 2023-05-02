c131329dc5457206dcfea08fcf9685d4225c6a72
SamuelAlvesNavarro <samuelalvesnavarro1@gmail.com>
2023-05-02 09:19:12 -0300

    bnd

---
+++ revisao.sql
@@ -0,0 +1,16 @@
+-- COMANDOS SQL SE DIVIDEM EM DUAS LINGUAGENS
+-- -----------------------------------------------------
+-- DDL (Data Definition Languagem)
+-- => Comandos de definiçãio de banco de dados
+-- Ex: CREATE DATABASE NAME 
+-- DROP DATABASE NAME
+-- CREATE TABLE NAME (COLUNAS)
+-- DROP TABLE NAME 	
+
+-- ----------------------------------------------------
+-- DML (Data Manipulation Language)
+-- => Comandos de manipulação de dados
+-- EX: SELECT COLUNAS FROM TABELA
+-- INSERT INTO TABELA (COLUNAS) VALUES (VALORES)
+-- DELETE FROM TABELA
+-- UPDATE COLUNAS FROM TABELA
