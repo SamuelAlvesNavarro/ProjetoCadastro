30923b615cfb2e4819c02163489b104f266305a5
SamuelAlvesNavarro <samuelalvesnavarro1@gmail.com>
2023-05-02 09:34:10 -0300

    bnd

--- Banco.sql
+++ Banco.sql
@@ -1,1 +1,13 @@
- -- Arquivo de criação --
+ -- Arquivo de criação do banco de dados--
+
+DROP DATABASE IF EXISTS projeto1;
+CREATE DATABASE projeto1;
+USE projeto1;
+
+CREATE TABLE usuario(
+    id_usuario int(4) primary key auto_increment,
+    nome varchar(50) not null,
+    email varchar(50) not null,
+    fone varchar(30) null,
+    senha varchar(30) not null
+);
